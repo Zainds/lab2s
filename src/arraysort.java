b8b756ce328a071b76cc326073168218d0a42227
Ангальдт Михаил <beat1097@mail.ru>
2023-02-13 23:13:00 +0700

    hh

---
+++ src/arraysort.java
@@ -0,0 +1,18 @@
+import javax.xml.transform.Templates;
+
+public class arraysort {
+
+
+
+    public static void main(String[] args){
+        int[][] arr = {
+                {1, 2, 3},
+                {3, 4, 1},
+                {3, 1, 0},
+                {4, 2, 6}  };
+
+
+
+    }
+
+}
--- src/task2.java
+++ src/task2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -5,6 +7,38 @@ public class task2 {
     public static int RandomRange(int min, int max){
         int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
         return randomNum;
+    }
+    public static void delete0rows(int[][] arr, int n, int k){
+        int rows = 0;
+        for (int i = 0; i<n; i++){
+            int multilp = 1;
+            for (int j = 0; j<k; j++){
+                multilp *= arr[i][j];
+            }
+            if (multilp != 0) rows +=1;
+        }
+        int[][] arr2 = new int[rows][k];
+        int rowback = 0;
+        for (int i = 0; i<n; i++){
+            int multilp = 1;
+            for (int j = 0; j<k; j++){
+                multilp *= arr[i][j];
+            }
+            if (multilp != 0){
+                for (int j = 0; j<k; j++){
+                    arr2[i + rowback][j] = arr[i][j];
+                }
+            }
+            else rowback -=1;
+        }
+
+        for (int i = 0; i< rows; i++){
+            for (int j = 0; j< k; j++){
+                System.out.print("   " + arr2[i][j]);
+            }
+            System.out.println();
+        }
+
     }
     public static void main(String[] args){
         int n, k;
@@ -17,7 +51,7 @@ public class task2 {
         for (int i = 0; i< n; i++){
             int flag = 1;
             for (int j = 0; j< k; j++){
-                arr[i][j] = RandomRange(1, 5);
+                arr[i][j] = RandomRange(2, 5);
                 System.out.print("   " + arr[i][j]);
                 if (arr[i][j]<4)flag = 0;
             }
@@ -28,14 +62,11 @@ public class task2 {
                 }
             }
         }
-        System.out.println();
-        for (int i = 0; i< n; i++){
-            for (int j = 0; j< k; j++){
-                System.out.print("   " + arr2[i][j]);
-            }
-            System.out.println();
 
-        }
+        System.out.println();
+        delete0rows(arr2, n, k);
+
+
     }
 }
 //

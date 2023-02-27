541c7f16de11f433cd5a9483ccf38b6c77d7ec7b
Ангальдт Михаил <beat1097@mail.ru>
2023-02-27 21:19:25 +0700

    task1 fix

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int n = 6;
-        int min_ind = -999;
+        int min_ind = 0;
         double sr = 0; int k = 0;
         int[] arr = new int[n];
         System.out.println("Enter day temperature");
         Scanner in = new Scanner(System.in);
         for (int i = 0; i < n; i ++){
             arr[i] = in.nextInt();
-            if (min_ind < arr[i]){
+            if (arr[i] < arr[min_ind]){
                 min_ind = i;
             }
         }
-        for (int i = 0; i<= min_ind; i++){
+
+        for (int i = 0; i< min_ind; i++){
             sr += arr[i];
             k++;
         }
